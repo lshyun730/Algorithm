2f08e9910274768da011b46fc997a5ae88e93022
lshyun730 <lshyun730@gmail.com>
2022-05-26 22:09:03 +0900

    220526 : backjoon

---
+++ backjoon/2309.whitePrincess.js
@@ -0,0 +1,17 @@
+function solution(...nums) {
+    nums.map((i) => parseInt(i));
+    let arr;
+    for (let i = 0; i < 8; i++) {
+        for (let j = i + 1; j < 9; j++) {
+            if (nums.reduce((sum, item) => sum + item, 0) === nums[i] + nums[j] + 100) {
+                arr = nums.filter((item) => item !== nums[i] && item !== nums[j]);
+                break;
+            }
+        }
+        if (!!arr) break;
+    }
+    arr.sort((a, b) => a - b);
+    for (let i = 0; i < 7; i++) console.log(arr[i]);
+}
+
+solution(20, 7, 23, 19, 10, 15, 25, 8, 13);
